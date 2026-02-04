ff2dfe4b5006a9eb192308099875e47cd07da562
Kai <oarkaiw6211@gmail.com>
2026-02-04 23:05:50 +0900

    add

--- Blind75/Easy/hasCycle.rb
+++ Blind75/Easy/hasCycle.rb
@@ -12,12 +12,14 @@
 
 
 # うさぎとかめのアルゴリズムを使用
+#うさぎは２歩進み、亀は1歩進む
+# 連結リスト（Linked List）が与えられたとき、サイクルがあるか確認する問題
 def hasCycle(head)
     return false if head.nil? || head.next.nil?
-
+# サイクルがない時にfastがnullになる
     slow = head
     fast = head
-
+#サイクルがある時はfastとslowが同じ場所に到達する
     while fast && fast.next # fastがnullでないかつfast.nextがnullでない限り
         slow = slow.next  #headを一個進める
         fast = fast.next.next #headを二個進める
