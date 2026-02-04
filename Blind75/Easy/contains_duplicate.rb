c4d6aae57f19c1a9d28134e46c50effe69bfbb49
Kai <oarkaiw6211@gmail.com>
2026-02-04 23:05:36 +0900

    update

--- Blind75/Easy/contains_duplicate.rb
+++ Blind75/Easy/contains_duplicate.rb
@@ -2,4 +2,7 @@
 # @return {Boolean}
 def contains_duplicate(nums)
     nums.uniq.length != nums.length
+    # uniqは重複を削除するメソッド
+    # lengthは配列の長さを返す
+    # したがって、重複を削除した配列の長さが元の配列の長さと異なれば、重複が存在する
 end
