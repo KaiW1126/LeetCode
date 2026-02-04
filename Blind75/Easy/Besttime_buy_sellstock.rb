f8e56538ab8df263eee9f50a3e5ff9d14312a3bd
Kai <oarkaiw6211@gmail.com>
2026-02-04 23:05:29 +0900

    feat: Update

--- Blind75/Easy/Besttime_buy_sellstock.rb
+++ Blind75/Easy/Besttime_buy_sellstock.rb
@@ -1,12 +1,13 @@
 # @param {Integer[]} prices
 # @return {Integer}
 def max_profit(prices)
-    min_price = Float::INFINITY #minpriceには今まで配列でみた中で一番小さいものを入れたいからとりあえずこの世で一番でかいもの入れとく
+    min_price = Float::INFINITY 
+    #minpriceには今まで配列でみた中で一番小さいものを入れたいからとりあえずこの世で一番でかいもの入れとく
     max_profit = 0
 
     prices.each do |price|
         min_price = [min_price, price].min
-        #.minしたら初期値無限大なんだから絶対priceが一回めに入るようになる
+        # minしたら初期値無限大なんだから絶対priceが一回めに入るようになる
         max_profit = [max_profit, price - min_price].max
     end
     max_profit
