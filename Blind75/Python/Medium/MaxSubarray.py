49ceb4bfac6123041b1652e982f82730ff19883b
Kai <oarkaiw6211@gmail.com>
2026-02-14 20:16:44 +0900

    51

---
+++ Blind75/Python/Medium/MaxSubarray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        cur_sum = 0
+        max_sum = float('-inf')
+        for num in nums:
+            cur_sum += num
+            max_sum = max(max_sum, cur_sum)
+            if cur_sum < 0:
+                cur_sum = 0
+        return max_sum
