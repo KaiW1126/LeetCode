67e2901367fc1a7f1a69762fd6d7b94d7bb53bdd
Kai <oarkaiw6211@gmail.com>
2026-02-16 23:24:32 +0900

    73

---
+++ Blind75/Python/Medium/SetMatrixZeros.py
@@ -0,0 +1,15 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        zero_positions = []
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    zero_positions.append((i, j))
+        for i, j in zero_positions:
+            for k in range(len(matrix)):
+                matrix[k][j] = 0
+            for k in range(len(matrix[0])):
+                matrix[i][k] = 0
